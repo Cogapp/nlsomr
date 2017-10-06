39e6c2ba2ab57dc186458ff5ed8b86684965a9db
stephen norris <stephenn@stephens-iMac.local>
2017-10-06 14:13:35 +0100

    scan directories

--- processor/processor.py
+++ processor/processor.py
@@ -1,24 +1,25 @@
 import os
+from os import listdir
 import sys
 
 audiveris_cli_path = "/Users/stephenn/Documents/workspace/audiveris/build/distributions/Audiveris/bin/"
 
-inputfolder = "/Users/stephenn/Documents/workspace/nlsomr/images/91386487/"
-outputfolder = "/Users/stephenn/Documents/workspace/nlsomr/xml_out"
-
-inputfile = "91386960.jpg"
-workingfile = "%stmp.jpg" % (inputfolder)
+baseFolder = "/Users/stephenn/Documents/workspace/nlsomr/"
+inputFolder = baseFolder+"images/"
+outputFolder = baseFolder+"xml_out/"
+workingFolder = baseFolder+"working/"
 
 
 if __name__ == '__main__':
     #scan input dir
 
     #recursive(or at least 2 stage ) loop
-
-    #upscale image with imagemagick
-    print workingfile 
-    #print "convert %s%s" % (inputfolder, inputfile)
-    os.system("convert %s%s   -resize 200%%  %s" % (inputfolder, inputfile, workingfile))
-
-    #run omr
-    os.system("%saudiveris -batch -export -output \"%s\" -- \"%s\"" % (audiveris_cli_path, outputfolder, workingfile))
+    #just assuming folder format of books as top level subdirs containing individual pages as images 
+    for book in listdir(inputFolder):
+        print "BOOK:"+book
+        if not os.path.isfile(inputFolder + book):
+            for inputfile in listdir(inputFolder + book):
+                print "file:"+inputfile
+                #upscale image with imagemagick
+                os.system("convert %s%s/%s   -resize 200%%  %s%s" % (inputFolder,book,inputfile, workingFolder, inputfile))
+                os.system("%saudiveris -batch -export -output \"%s%s\" -- \"%s%s\"" % (audiveris_cli_path, outputFolder, book, workingFolder, inputfile))
