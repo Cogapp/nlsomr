f629b9d582211bb9e4196c0a48073db27bf10268
stephen norris <stephenn@stephens-iMac.local>
2017-10-06 15:29:54 +0100

    go on then tristan have some output

--- processor/processor.py
+++ processor/processor.py
@@ -12,14 +12,16 @@ workingFolder = baseFolder+"working/"
 
 if __name__ == '__main__':
     #scan input dir
-
-    #recursive(or at least 2 stage ) loop
     #just assuming folder format of books as top level subdirs containing individual pages as images 
     for book in listdir(inputFolder):
         print "BOOK:"+book
         if not os.path.isfile(inputFolder + book):
             for inputfile in listdir(inputFolder + book):
-                print "file:"+inputfile
-                #upscale image with imagemagick
-                os.system("convert %s%s/%s   -resize 200%%  %s%s" % (inputFolder,book,inputfile, workingFolder, inputfile))
-                os.system("%saudiveris -batch -export -output \"%s%s\" -- \"%s%s\"" % (audiveris_cli_path, outputFolder, book, workingFolder, inputfile))
+                if os.path.isfile(inputFolder+book+"/"+inputfile) and inputfile != ".DS_Store"
+                    print "file:"+inputfile
+                    #upscale image with imagemagick
+                    if not os.path.isfile(inputFolder + book): #hacky method of skipping files we have alreay processed
+                        os.system("convert %s%s/%s   -resize 200%%  %s%s > /dev/null" % (inputFolder,book,inputfile, workingFolder, inputfile))
+                        os.system("%saudiveris -batch -export -output \"%s%s\" -- \"%s%s\" > /dev/null" % (audiveris_cli_path, outputFolder, book, workingFolder, inputfile))
+                    for outputItem in listdir(outputFolder + book + os.path.splitext(inputfile)[0]):
+                        if os.path.splitext(inputfile)[0] == "mxl": #there may be multiple mxl files generated so we skip
