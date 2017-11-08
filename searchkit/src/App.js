fd60c27213d4bebfc48573283338aa16634da91b
stephen norris <stephenn@stephens-iMac.local>
2017-11-08 12:15:08 +0000

    move updates from iif-ml project over and fix basic search

--- searchkit/src/App.js
+++ searchkit/src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react'
+
+import {Helmet} from "react-helmet";
+import {  Route, Link, Switch } from 'react-router-dom'
+import { Collapse } from 'react-collapse';
+import { presets } from 'react-motion';
+import PropTypes from 'prop-types';
 import { extend } from 'lodash'
 import ReactModal from 'react-modal'
 import { SearchkitManager,SearchkitProvider,
@@ -11,9 +17,12 @@ import { SearchkitManager,SearchkitProvider,
   ActionBar, ActionBarRow, SideBar } from 'searchkit'
 import './index.css'
 
-//const host = "http://demo.searchkit.co/api/movies"
-const host = "http://e.cogapp.com:80/nls-omr"
-const searchkit = new SearchkitManager(host)
+const host = "http://e.cogapp.com/nls-omr"
+const searchkit = new SearchkitManager(host, {
+  timeout: 50000
+  // basicAuth:"elastic:changeme"
+})
+const baseUrl = process.env.PUBLIC_URL;
 
 const MovieHitsGridItem = (props)=> {
   const {bemBlocks, result} = props
@@ -56,24 +65,54 @@ const MovieHitsListItem = (props)=> {
   )
 }
 
-const Modal = () => {
-  <ReactModal isOpen={this.state.showModal} contentLabel="About" className="Modal" overlayClassName="Overlay" style={modalStyles}>
-    <h2>About this project</h2>
-    <p>This is a hackday project from <a href="https://cogapp.com">Cogapp</a> in association with <a href="http://nls.org.uk">The National Library of Scotland</a>, produced as part of Coghack3, Sept 2017</p>
-    <p>The project is an investigation into the use of Optical Musical Recognition software to extract meaning from musical scores in the archive.</p>
-    <p>All source material is taken from the <a href="https://blog.cogapp.com/from-machine-learning-to-human-learning-ebddd6a9929a">antiquarian books archive</a> at the National Library of Scotland</p>
-    <p>Software used in this project is as follows:</p>
-    <ul>
-    	<li>Image pre-processing: <a href="https://www.imagemagick.org">imagemagick</a></li>
-    	<li>Optical Musical Recognition: <a href="https://github.com/audiveris">Audiveris</a></li>
-    	<li>MusicXML to Wav conversion: <a href="https://musescore.org/">MuseScore</a></li>
-    	<li>Search Engine: <a href="http://www.elastic.co/">Elasticsearch</a></li>
-    	<li>Front end framework: <a href="http://www.searchkit.co/">Searchkit</a></li>
-    </ul>
-    <button onClick={this.handleCloseModal}>Close</button>
-  </ReactModal>
-}
+const modal_html = '    <h2>About this project</h2>' +
+'    <p>This is a hackday project from <a href="https://cogapp.com">Cogapp</a> in association with <a href="http://nls.org.uk">The National Library of Scotland</a>, produced as part of Coghack3, Sept 2017</p>' +
+'    <p>The project is an investigation into the use of Optical Musical Recognition software to extract meaning from musical scores in the archive.</p>' +
+'    <p>All source material is taken from the <a href="https://blog.cogapp.com/from-machine-learning-to-human-learning-ebddd6a9929a">antiquarian books archive</a> at the National Library of Scotland</p>' +
+'    <p>Best viewed with the Google Chrome browser on desktop</p>' +
+'    <p>Software used in this project is as follows:</p>' +
+'    <ul>' +
+'    	<li>Image pre-processing: <a href="https://www.imagemagick.org">imagemagick</a></li>' +
+'    	<li>Optical Musical Recognition: <a href="https://github.com/audiveris">Audiveris</a></li>' +
+'    	<li>MusicXML to Wav conversion: <a href="https://musescore.org/">MuseScore</a></li>' +
+'    	<li>Search Engine: <a href="http://www.elastic.co/">Elasticsearch</a></li>' +
+'    	<li>Front end framework: <a href="http://www.searchkit.co/">Searchkit</a></li>' +
+'    </ul>';
 
+
+class AboutText extends React.Component {
+  static propTypes = {
+    isOpened: PropTypes.bool
+  };
+
+
+  static defaultProps = {
+    isOpened: false
+  };
+
+
+  constructor(props) {
+    super(props);
+    this.state = {isOpened: this.props.isOpened};
+  }
+
+  render() {
+    const {isOpened} = this.state;
+
+    return (
+      <div className="modal-wrapper">
+        <div>
+          <span className="button modal-button" onClick={() => this.setState({isOpened: !isOpened})}>{!isOpened ? "About this project" : "Close"}</span>
+        </div>
+
+        <Collapse isOpened={isOpened}>
+          <div className="modal-content" dangerouslySetInnerHTML={{__html: modal_html}}>
+          </div>
+        </Collapse>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   constructor () {
@@ -100,23 +139,7 @@ class App extends Component {
         <Layout>
           <TopBar>
             <div className="my-logo"><a href="https://cogapp.com"><img src="cogapp-logo.jpg" alt="Cogapp.com" /></a></div>
-            <SearchBox autofocus={true} searchOnChange={true} queryFields={["page_id"]}/>
-            <ReactModal isOpen={this.state.showModal} contentLabel="About"className="Modal" overlayClassName="Overlay" style={modalStyles}>
-              <h2>About this project</h2>
-				<p>This is a hackday project from <a href="https://cogapp.com">Cogapp</a> in association with <a href="http://nls.org.uk">The National Library of Scotland</a>, produced as part of Coghack3, Sept 2017</p>
-				<p>The project is an investigation into the use of Optical Musical Recognition software to extract meaning from musical scores in the archive.</p>
-				<p>All source material is taken from the <a href="https://blog.cogapp.com/from-machine-learning-to-human-learning-ebddd6a9929a">antiquarian books archive</a> at the National Library of Scotland</p>
-				<p>Software used in this project is as follows:</p>
-				<ul>
-					<li>Image pre-processing: <a href="https://www.imagemagick.org">imagemagick</a></li>
-					<li>Optical Musical Recognition: <a href="https://github.com/audiveris">Audiveris</a></li>
-					<li>MusicXML to Wav conversion: <a href="https://musescore.org/">MuseScore</a></li>
-					<li>Search Engine: <a href="http://www.elastic.co/">Elasticsearch</a></li>
-					<li>Front end framework: <a href="http://www.searchkit.co/">Searchkit</a></li>
-				</ul>
-              <button onClick={this.handleCloseModal}>Close</button>
-
-            </ReactModal>
+            <SearchBox autofocus={true} searchOnChange={true} />
           </TopBar>
 
         <LayoutBody>
@@ -136,13 +159,14 @@ class App extends Component {
                <ActionBarRow>
                 <GroupedSelectedFilters/>
                 <ResetFilters/>
-                <div className="about-link"><a onClick={this.handleOpenModal}>About this project</a></div>
+                <AboutText />
               </ActionBarRow>
 
 
             </ActionBar>
             <ViewSwitcherHits
-                hitsPerPage={50} highlightFields={["title","plot"]}
+                hitsPerPage={50} 
+                //highlightFields={["title","plot"]}
                 //sourceFilter={["plot", "title", "poster", "imdbId", "imdbRating", "year"]}
                 hitComponents={[
                   {key:"grid", title:"Grid", itemComponent:MovieHitsGridItem, defaultOption:true},
@@ -150,7 +174,7 @@ class App extends Component {
                 ]}
                 scrollTo="body"
             />
-            <NoHits suggestionsField={"title"}/>
+            <NoHits suggestionsField={"page.children.page_id"} />
             <Pagination showNumbers={true}/>
           </LayoutResults>
 
